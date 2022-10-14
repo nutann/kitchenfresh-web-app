bb0933e434feb16407a67efa5fc3e3760882398e
Nutan Naik <nutann.naik@honeywell.com>
2022-10-14 13:05:26 +0530

    Clean up

--- src/App.js
+++ src/App.js
@@ -18,17 +18,6 @@ import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-// function App({ signOut, user }) {
-//   return (
-//     <>
-//       <h1>Hello {user.username}</h1>
-//       <button onClick={signOut}>Sign out</button>
-//     </>
-//   );
-// }
-
-// export default withAuthenticator(App);
-
 
 class App extends Component {
 
